9bd8d7928bf026abbcceb56e3663b21db438d220
Chariyaporn Umpornpan <chariyaporn.u@gmail.com>
2024-02-09 07:31:22 +0700

    Create report.js

---
+++ Report/report.js
@@ -0,0 +1,43 @@
+// Incident Report Device
+
+const io = require('socket.io-client');
+
+// mock
+const port = process.argv[2];
+const id = process.argv[3];
+
+let latitude;
+let longitude;
+
+// RSU data
+let rsuIp = 'localhost'; // mock
+let rsuPort = 8000; // mock
+
+// report type
+const TYPE = ['ACCIDENT', 'CLOSED ROAD', 'CONSTRUCTION', 'TRAFFIC CONGESTION'];
+
+// connect to RSU
+const socket = io(`http://${rsuIp}:${rsuPort}`);
+
+// socket (connected to RSU)
+socket.on('connect', () => {
+	console.log('Connected to the server');
+});
+
+socket.on('disconnect', () => {
+	console.log('Disconnected from the server');
+});
+
+// mock report
+const emitReport = setInterval(() => {
+	message = {
+		type: TYPE[Math.floor(Math.random() * 4)],
+		detail: 'mock incident report',
+		latitude: latitude,
+		longitude: longitude,
+		timestamp: Date(),
+	};
+	socket.emit('incident report', (message) => {
+		console.log('Sent incident:', message);
+	});
+}, 10000);
