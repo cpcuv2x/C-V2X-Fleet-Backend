a7385451e8ea2abc7c793275202ac45dd34c120b
Palm394 <palm.nichapat@gmail.com>
2024-02-11 22:31:41 +0700

    Initialize package for simulator

--- simulator/server/index.js
+++ simulator/server/index.js
@@ -1,7 +1,31 @@
+const app = require('express')();
+const cors = require('cors');
 const { fork } = require('child_process');
-const path = require('path');
 
-const child_process = fork(path.join(__dirname, 'obu.js'), ['12345', 'ID1']);
-fork(path.join(__dirname, 'obu.js'), ['12346', 'ID2']);
+const child_process = fork('obu.js', ['12345', 'ID1']);
+const child_process1 = fork('obu.js', ['12346', 'ID2']);
+// child_process.send(20);
 
-child_process.send(20);
+const OBU_MAP = new Map();
+OBU_MAP.set('ID1', {
+	id: 'ID1',
+	speed: 20,
+});
+OBU_MAP.set('ID2', {
+	id: 'ID2',
+	speed: 20,
+});
+
+app.use(cors());
+
+app.get('/obu', (req, res) => {
+	res.json(Array.from(OBU_MAP));
+});
+
+app.get('/obu/:id', (req, res) => {
+	res.json(OBU_MAP.get(req.params.id));
+});
+
+app.listen(3425, () => {
+	console.log('Simulator is running on port 3425');
+});
