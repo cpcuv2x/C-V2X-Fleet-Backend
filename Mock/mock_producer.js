beecffecf70e91399f5c841c8bb44c1fc6b4b9a2
Chariyaporn Umpornpan <chariyaporn.u@gmail.com>
2024-02-20 08:48:43 +0700

    update mock producer to only publish rec speed when receiving heartbeat

--- Mock/mock_producer.js
+++ Mock/mock_producer.js
@@ -1,14 +1,34 @@
 const Producer = require('../RabbitMQ/producer');
+const Consumer = require('../RabbitMQ/consumer');
 
 const producer = Producer();
 producer.connect();
 
-setInterval(() => {
-	message = JSON.stringify({
-		id: '01',
-		velocity: Math.random() * 120,
-		unit: 'km/h',
-		timestamp: Date(),
-	});
-	producer.publish('recommend_speed_rsu_01', message);
-}, 2000);
+const publishRecSpeed = (msg) => {
+	let id = msg['id'];
+	if (id === '01') {
+		message = JSON.stringify({
+			id: '01',
+			velocity: Math.random() * 120,
+			unit: 'km/h',
+			timestamp: Date(),
+		});
+		producer.publish('recommend_speed_rsu_01', message);
+	}
+};
+
+const consumer = Consumer(
+	'queue_heartbeat_rsu',
+	'heartbeat_rsu',
+	publishRecSpeed,
+);
+
+// setInterval(() => {
+// 	message = JSON.stringify({
+// 		id: '01',
+// 		velocity: Math.random() * 120,
+// 		unit: 'km/h',
+// 		timestamp: Date(),
+// 	});
+// 	producer.publish('recommend_speed_rsu_01', message);
+// }, 2000);
