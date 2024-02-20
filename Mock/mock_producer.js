5baf03dde2ff6c24a7c60409e0511cbcbef64f2c
Chariyaporn Umpornpan <chariyaporn.u@gmail.com>
2024-02-20 15:42:39 +0700

    edit `mock_producer`

--- Mock/mock_producer.js
+++ Mock/mock_producer.js
@@ -1,10 +1,11 @@
 const Producer = require('../RabbitMQ/producer');
 const Consumer = require('../RabbitMQ/consumer');
 
-const producer = Producer();
+const producer = Producer('rec_speed_01', true);
 producer.connect();
 
 const publishRecSpeed = (msg) => {
+	console.log(msg);
 	let id = msg['id'];
 	if (id === '01') {
 		message = JSON.stringify({
@@ -13,15 +14,11 @@ const publishRecSpeed = (msg) => {
 			unit: 'km/h',
 			timestamp: Date(),
 		});
-		producer.publish('recommend_speed_rsu_01', message);
+		producer.publish(message);
 	}
 };
 
-const consumer = Consumer(
-	'queue_heartbeat_rsu',
-	'heartbeat_rsu',
-	publishRecSpeed,
-);
+const consumer = Consumer('heartbeat', publishRecSpeed);
 
 // setInterval(() => {
 // 	message = JSON.stringify({
