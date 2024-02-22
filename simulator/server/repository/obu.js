cad5125b10cea9cc5cdb71539eb1cbea46144473
Palm394 <palm.nichapat@gmail.com>
2024-02-22 21:13:09 +0700

    simulator:  entity repo

--- simulator/server/repository/obu.js
+++ simulator/server/repository/obu.js
@@ -1,14 +1,34 @@
 function getOBUdata() {
 	const data = [
 		{
-			id: '658bd8906cc3ce49493b5ffd',
+			id: '652b3a1dcb4027dd518885d0',
 			port: '12345',
-			name: 'Car1',
+			name: 'car02',
 		},
 		{
-			id: '65928d6c4ddcbab56e50c72c',
+			id: '6528b90b9417d272c4ef49b1',
 			port: '12346',
-			name: 'Car2',
+			name: 'car03',
+		},
+		{
+			id: '65ac9747191a85b6842de0fc',
+			port: '12347',
+			name: 'car04',
+		},
+		{
+			id: '65ac977e191a85b6842de10d',
+			port: '12348',
+			name: 'car05',
+		},
+		{
+			id: '65ac9788191a85b6842de111',
+			port: '12349',
+			name: 'car06',
+		},
+		{
+			id: '65ac9791191a85b6842de115',
+			port: '12350',
+			name: 'car07',
 		},
 	];
 	return data;
--- simulator/server/repository/rsu.js
+++ simulator/server/repository/rsu.js
@@ -1,14 +1,14 @@
 function getRSUdata() {
 	const data = [
 		{
-			id: '65950739a29a088c0b9b31e9',
-			port: '12347',
-			name: 'RSU_1',
+			id: '65c6387e77590d0d069dcdb3',
+			port: '32347',
+			name: 'RSU01',
 		},
 		{
-			id: '65a538a9f9f2eaf4fabf4526',
-			port: '12348',
-			name: 'RSU_2',
+			id: '65c6389877590d0d069dcdba',
+			port: '32348',
+			name: 'RSU02',
 		},
 	];
 	return data;
