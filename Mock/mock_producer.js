16fa909bd5648c1e69b0d4aafe9c61d7a5f4b3fe
Chariyaporn Umpornpan <chariyaporn.u@gmail.com>
2024-03-07 07:23:47 +0700

    edit mock producer for rsu01

--- Mock/mock_producer.js
+++ Mock/mock_producer.js
@@ -1,16 +1,16 @@
 const Producer = require('../RabbitMQ/producer');
 const Consumer = require('../RabbitMQ/consumer');
 
-const producer = Producer('rec_speed_01', true);
+const producer = Producer('rec_speed_65c6387e77590d0d069dcdb3', true);
 producer.connect();
 
 const publishRecSpeed = (msg) => {
 	console.log(msg);
 	let id = msg['id'];
-	if (id === '01') {
+	if (id === '65c6387e77590d0d069dcdb3') {
 		message = JSON.stringify({
-			id: '01',
-			velocity: Math.random() * 120,
+			id: '65c6387e77590d0d069dcdb3',
+			recommend_speed: Math.random() * 120,
 			unit: 'km/h',
 			timestamp: new Date(),
 		});
