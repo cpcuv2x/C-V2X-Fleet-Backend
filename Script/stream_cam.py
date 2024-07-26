45737adf65d37dd93af36ad757e1399db257f0fe
Warayut Kaenkaew <m_laker42@hotmail.com>
2024-07-26 16:14:54 +0700

    Update the streaming code to work with 20 fpx

--- Script/stream_cam.py
+++ Script/stream_cam.py
@@ -20,19 +20,19 @@ def dont_stop(name, command):
             time.sleep(3)
 
 
-IP = "192.168.10.86:8554"
-STREAM = ["stream1", "stream2", "stream3", "stream4"]
+IP = "root:smrc@192.168.1.90"
+STREAM = ["axis-media/media.amp?camera=1", "axis-media/media.amp?camera=2", "axis-media/media.amp?camera=3", "axis-media/media.amp?camera=4"]
 VIDEO = ["video0", "video2", "video4", "video6"]
 
 
 command1 = shlex.split(
-    f'ffmpeg -rtsp_transport tcp -stimeout 100 -i rtsp://{IP}/{STREAM[0]} -fflags nobuffer -pix_fmt rgb24 -f v4l2 /dev/{VIDEO[0]}')
+    f'ffmpeg -rtsp_transport tcp -stimeout 100 -i rtsp://{IP}/{STREAM[0]} -fflags nobuffer -filter:v fps=20 -pix_fmt rgb24 -f v4l2 /dev/{VIDEO[0]}')
 command2 = shlex.split(
-    f'ffmpeg -rtsp_transport tcp -stimeout 100 -i rtsp://{IP}/{STREAM[1]} -fflags nobuffer -pix_fmt rgb24 -f v4l2 /dev/{VIDEO[1]}')
+    f'ffmpeg -rtsp_transport tcp -stimeout 100 -i rtsp://{IP}/{STREAM[1]} -fflags nobuffer -filter:v fps=20 -pix_fmt rgb24 -f v4l2 /dev/{VIDEO[1]}')
 command3 = shlex.split(
-    f'ffmpeg -rtsp_transport tcp -stimeout 100 -i rtsp://{IP}/{STREAM[2]} -fflags nobuffer -pix_fmt rgb24 -f v4l2 /dev/{VIDEO[2]}')
+    f'ffmpeg -rtsp_transport tcp -stimeout 100 -i rtsp://{IP}/{STREAM[2]} -fflags nobuffer -filter:v fps=20 -pix_fmt rgb24 -f v4l2 /dev/{VIDEO[2]}')
 command4 = shlex.split(
-    f'ffmpeg -rtsp_transport tcp -stimeout 100 -i rtsp://{IP}/{STREAM[3]} -fflags nobuffer -pix_fmt rgb24 -f v4l2 /dev/{VIDEO[3]}')
+    f'ffmpeg -rtsp_transport tcp -stimeout 100 -i rtsp://{IP}/{STREAM[3]} -fflags nobuffer -filter:v fps=20 -pix_fmt rgb24 -f v4l2 /dev/{VIDEO[3]}')
 
 ps1 = threading.Thread(target=dont_stop, args=["Front", command1])
 ps2 = threading.Thread(target=dont_stop, args=["Back", command2])
